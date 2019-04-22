8833dd70f5d39616d1457250705c2628e7bccd4b
berio <beries@gmail.com>
2019-04-22 16:50:29 +0200

    Xa non necesitamos conditionizr nin modernizr

--- functions.php
+++ functions.php
@@ -92,16 +92,10 @@ function laulo_header_scripts()
 {
     if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
 
-    	wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0'); // Conditionizr
-        wp_enqueue_script('conditionizr'); // Enqueue it!
-
-        wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
-        wp_enqueue_script('modernizr'); // Enqueue it!
-
         wp_register_script('bootstrap_js', get_template_directory_uri() . '/js/lib/bootstrap.min.js', array(), '4.0'); // Modernizr
-
         wp_register_script('lauloscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'bootstrap_js'), '1.0.0'); // Custom scripts
-        wp_enqueue_script('lauloscripts'); // Enqueue it!
+        
+	wp_enqueue_script('lauloscripts'); // Enqueue it!
     }
 }
 
