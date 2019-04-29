3ba7f6c5b8e5b088f519c3d4a84d48201f1ffab7
Berio <beries@gmail.com>
2019-04-29 14:13:19 +0200

    Engadido footer menu e limpeza

--- functions.php
+++ functions.php
@@ -109,8 +109,7 @@ function register_html5_menu()
 {
     register_nav_menus(array( // Using array to specify more menus if needed
         'header-menu' => __('Header Menu', 'laulo'), // Main Navigation
-        'sidebar-menu' => __('Sidebar Menu', 'laulo'), // Sidebar Navigation
-        'extra-menu' => __('Extra Menu', 'laulo') // Extra Navigation if needed (duplicate as many as you need!)
+        'footer-menu' => __('Footer Menu', 'laulo') // Extra Navigation if needed (duplicate as many as you need!)
     ));
 }
 
